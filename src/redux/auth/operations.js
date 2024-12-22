964bdfb4c5bbc0dfd1783e538500b9f8411c482b
liahychka <157300297+liahychka@users.noreply.github.com>
2024-12-22 20:34:35 +0100

    Update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const generalApi = axios.create({
-    baseURL: 'http://localhost:3000/',
+    baseURL: 'https://connections-api.goit.global',
 });
 
 export const setAuthHeaders = (token) => {
